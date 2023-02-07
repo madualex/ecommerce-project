f36eba5295a0ce004a5786195e02bec1457175c2
alexmadu <ogochukwumadu.ng@gmail.com>
2023-02-07 14:25:55 +0100

    first commit

---
+++ fashion/schemas/index.js
@@ -0,0 +1,15 @@
+import banner from "./banner";
+import product from "./product";
+
+export const schemaTypes = [product, banner]
+
+// import createSchema from 'part:@sanity/base/schema-creator';
+// import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+// import product from './product';
+// import banner from './banner';
+
+// export default createSchema({
+//     name: 'default',
+//     types: schemaTypes.concat([ product, banner ]),
+// })
---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const Home = ({ productData, bannerData }) => {
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div className='products-container'>
+        {
+          productData?.map((product) => <Product key={product._id} product={product} />)
+        }
+      </div>
+
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    </>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const productQuery = `*[_type == "product"]`;
+  const productData = await client.fetch(productQuery);
+
+  const bannerQuery = `*[_type == "banner"]`;
+  const bannerData = await client.fetch(bannerQuery);
+
+
+  return {
+    props: { productData, bannerData }
+  }
+}
+
+
+export default Home
